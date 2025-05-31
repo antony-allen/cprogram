3b1439f50ff27a9fd94ff8f01bfedefa9a7c2039
antony-allen <liangxuan.antony@zohomail.com>
2025-05-31 10:47:26 +0800

    更新到函数习题，练习题至99乘法表。
    Signed-off-by: antony-allen <liangxuan.antony@zohomail.com>

---
+++ add.h
@@ -0,0 +1,9 @@
+//
+// Created by 86188 on 2025/6/22.
+//17.6、函数的声明和定义
+
+#ifndef CPROGRAM_ADD_H
+#define CPROGRAM_ADD_H
+
+//int add(int x, int y);                            //函数的声明
+#endif //CPROGRAM_ADD_H
